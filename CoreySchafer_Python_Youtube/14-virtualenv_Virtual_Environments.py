531bc0a1cf74bcdaa1d2ce028a7c6f28dc8634e5
ShukuratBello <belloshukurat03@gmail.com>
2023-09-10 23:44:52 +0100

    Day 10 - Virtual Environment in Python

---
+++ CoreySchafer_Python_Youtube/14-virtualenv_Virtual_Environments.py
@@ -0,0 +1,12 @@
+# Using Virtual environments to manage python projects and packages and versions
+# pip install virtualenv
+# mkdir Environments
+# cd !$ - to go to the directory just created
+# virtualenv project1_env - To create a virtual environment
+# Xsource project1_env/bin/activate - To activate the virtual environment
+# project1_env\Scripts\activate
+# which pip X gcm Python
+# pip list
+# pip install numpy
+# pip install pytz
+# pip install psutil
